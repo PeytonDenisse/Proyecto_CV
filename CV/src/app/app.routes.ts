6912f205bbce3cb881998ae2d4585c7815ee3a26
Denisse Chagoya <denissechagoyaveloz@gmail.com>
2025-08-26 11:20:37 -0600

    rutas

--- CV/src/app/app.routes.ts
+++ CV/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home').then(m => m.HomeComponent)
+  },
+  {
+    path: 'projects',
+    loadComponent: () => import('./pages/projects/projects').then(m => m.ProjectsComponent)
+  },
+  {
+    path: 'cv',
+    loadComponent: () => import('./pages/cv/cv').then(m => m.CvComponent)
+  },
+  { path: '**', redirectTo: 'home' }
+];
---
+++ CV/src/app/pages/cv/cv.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-cv',
+  standalone: true,
+  templateUrl: './cv.html',
+  styleUrl: './cv.scss',
+})
+export class CvComponent {}
